fe94ad7924a274a74db2da21efd4cd4c0b175fc0
DesignMagnetic <michael.howarth@nice.org.uk>
2016-12-08 14:25:55 +0000

    Hover state color change on dropdown for NICE Services

--- NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
+++ NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|IMANDT\\mhowarth
 vti_modifiedby:SR|IMANDT\\mhowarth
-vti_timelastmodified:TR|08 Dec 2016 14:22:30 -0000
+vti_timelastmodified:TR|08 Dec 2016 14:25:32 -0000
 vti_timecreated:TR|08 Dec 2016 09:58:20 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|08 Dec 2016 14:18:12 -0000
-vti_cacheddtm:TX|08 Dec 2016 14:22:30 -0000
-vti_filesize:IR|46505
+vti_nexttolasttimemodified:TW|08 Dec 2016 14:22:30 -0000
+vti_cacheddtm:TX|08 Dec 2016 14:25:32 -0000
+vti_filesize:IR|46519
