0cc5221496a61816a7a4d7918242690b5b138a69
DesignMagnetic <michael.howarth@nice.org.uk>
2016-12-08 13:43:25 +0000

    change

--- NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
+++ NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|IMANDT\\mhowarth
 vti_modifiedby:SR|IMANDT\\mhowarth
-vti_timelastmodified:TR|08 Dec 2016 13:40:48 -0000
+vti_timelastmodified:TR|08 Dec 2016 13:43:17 -0000
 vti_timecreated:TR|08 Dec 2016 09:58:20 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|08 Dec 2016 13:38:53 -0000
-vti_cacheddtm:TX|08 Dec 2016 13:40:48 -0000
-vti_filesize:IR|46509
+vti_nexttolasttimemodified:TW|08 Dec 2016 13:43:11 -0000
+vti_cacheddtm:TX|08 Dec 2016 13:43:11 -0000
+vti_filesize:IR|46503
