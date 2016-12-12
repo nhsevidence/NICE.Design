c67f1f83eaf5b22c816a6bb03d264b4b220bfdb2
DesignMagnetic <michael.howarth@nice.org.uk>
2016-12-12 11:13:55 +0000

    standard and indicators example added

--- NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
+++ NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|IMANDT\\mhowarth
 vti_modifiedby:SR|IMANDT\\mhowarth
-vti_timelastmodified:TR|09 Dec 2016 11:06:05 -0000
+vti_timelastmodified:TR|12 Dec 2016 11:12:30 -0000
 vti_timecreated:TR|08 Dec 2016 09:58:20 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|08 Dec 2016 16:24:30 -0000
-vti_cacheddtm:TX|09 Dec 2016 11:06:05 -0000
+vti_nexttolasttimemodified:TW|09 Dec 2016 11:06:05 -0000
+vti_cacheddtm:TX|12 Dec 2016 11:12:30 -0000
 vti_filesize:IR|46571
