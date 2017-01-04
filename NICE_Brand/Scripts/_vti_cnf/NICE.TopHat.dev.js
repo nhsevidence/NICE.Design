b58424f49fcc6035e1f961371e0bd79ed82d33dd
DesignMagnetic <michael.howarth@nice.org.uk>
2017-01-04 11:42:55 +0000

    border bottom color change

--- NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
+++ NICE_Brand/Scripts/_vti_cnf/NICE.TopHat.dev.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|IMANDT\\mhowarth
 vti_modifiedby:SR|IMANDT\\mhowarth
-vti_timelastmodified:TR|30 Dec 2016 11:59:34 -0000
+vti_timelastmodified:TR|04 Jan 2017 11:42:40 -0000
 vti_timecreated:TR|08 Dec 2016 09:58:20 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|30 Dec 2016 11:54:54 -0000
-vti_cacheddtm:TX|30 Dec 2016 11:59:34 -0000
+vti_nexttolasttimemodified:TW|30 Dec 2016 11:59:34 -0000
+vti_cacheddtm:TX|04 Jan 2017 11:42:40 -0000
 vti_filesize:IR|46620
