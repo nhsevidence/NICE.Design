ca2db234fdb39014ad229daa59deef6cc674bcf5
DesignMagnetic <michael.howarth@nice.org.uk>
2017-02-02 14:20:35 +0000

    save

--- NICE_Brand/Scripts/_vti_cnf/jquery.museresponsive.js
+++ NICE_Brand/Scripts/_vti_cnf/jquery.museresponsive.js
@@ -1,9 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|01 Feb 2017 14:56:40 -0000
+vti_timelastmodified:TR|02 Feb 2017 14:20:30 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|IMANDT\\mhowarth
 vti_modifiedby:SR|IMANDT\\mhowarth
 vti_timecreated:TR|01 Feb 2017 14:56:40 -0000
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|01 Feb 2017 14:56:40 -0000
 vti_cacheddtm:TX|01 Feb 2017 14:56:40 -0000
 vti_filesize:IR|5881
-vti_backlinkinfo:VX|
