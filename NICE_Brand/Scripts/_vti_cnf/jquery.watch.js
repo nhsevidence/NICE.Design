130f32fcb62a5b884a42063b193d510b1c7b33bd
DesignMagnetic <michael.howarth@nice.org.uk>
2017-11-27 14:11:52 +0000

    something on the 23rd

--- NICE_Brand/Scripts/_vti_cnf/jquery.watch.js
+++ NICE_Brand/Scripts/_vti_cnf/jquery.watch.js
@@ -1,9 +1,9 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|31 Jan 2017 15:45:10 -0000
+vti_timelastmodified:TR|23 Nov 2017 16:06:12 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|IMANDT\\mhowarth
 vti_modifiedby:SR|IMANDT\\mhowarth
 vti_timecreated:TR|31 Jan 2017 15:45:10 -0000
-vti_cacheddtm:TX|31 Jan 2017 15:45:10 -0000
-vti_filesize:IR|1851
 vti_backlinkinfo:VX|
+vti_cacheddtm:TX|23 Nov 2017 16:06:12 -0000
+vti_filesize:IR|1851
